b1dd63b3d433c351bef2145bd92e99ae580a2d26
Ruthba Anam <ruthbaanam@gmail.com>
2019-08-21 11:05:50 -0400

    got graphdisplay to render

--- client/components/GraphDisplay.jsx
+++ client/components/GraphDisplay.jsx
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
-import GraphDisplay from './GraphDisplay.jsx'
 
-class Data extends Component {
+class GraphDisplay extends Component {
   render() {
     return (
-      <div className="outerbox">
-       <GraphDisplay />
+      <div className="innerbox">
+        HEY I AM FROM GraphDisplay.JSX
       </div>
     )
   }
 }
 
-export default Data;
+export default GraphDisplay;
