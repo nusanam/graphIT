ebbe54b5616caeb2b72bc822b5ad3768c3be3dc9
Ruthba Anam <ruthbaanam@gmail.com>
2019-08-21 17:10:03 -0400

    styling finalized

--- client/components/Data.jsx
+++ client/components/Data.jsx
@@ -12,15 +12,16 @@ class Data extends Component {
         <div className="ui divider" />
         <p>
           <span id="data">
-            X-Axis Data Labels: &nbsp;</span>
-          <input id="input" size="35" placeholder="[ 'Kiwis', 'Pears', 'Apples' , 'Limes' ]" />
+            X-axis Values: &nbsp;</span>
+          <input id="input" size="31" placeholder="[ 'Kiwis', 'Pears', 'Apples' , 'Limes' ]" />
           &nbsp; &nbsp;
           <button id="submitBtn">Submit</button>
-
-            <br />
-            <br />
-            Input x-axis labels as an array of strings, where each string represents one label for your data point.
-            <br />
+          <br />
+          <br />
+          <span id="example">
+            Input x-axis labels as an <u><strong>array</strong></u> of strings.
+            </span>
+          <br />
 
         </p>
 
@@ -28,30 +29,21 @@ class Data extends Component {
         <div className="ui divider" />
         <p>
           <span id="data">
-            Dataset: &nbsp;</span>
-          <input id="input" size="20" placeholder="[12, 20, 5, 17, 8, 12.5]" />
+            Single Dataset: &nbsp;</span>
+          <input id="input" size="30" placeholder="[12, 5, 20, null, 17, 8, 12.5, 800, 9]" />
           &nbsp;&nbsp;
           <button id="submitBtn">Submit</button>
-          <span id="example">
+          <div id="example">
             <br />
-            <br />
-            Submit <i>each set of data</i> at a time.
-          </span>
+            Submit one dataset at a time as an <u><strong>array</strong></u> of values. If a data value inside of a dataset doesn't exist, use null in its place inside the array.
+            <br /> <br />
+            <strong>NOTE: </strong> The number of labels must exactly <strong><u>equal</u></strong> the number of values in a single dataset.
+          </div>
         </p>
-          <div className="ui divider" />
-
-        {/* BELOW IS STRETCH FEATURE TO SELECT STORED GRAPHS */}
+        <div className="ui divider" />
 
         {/* Instantiating an instance of graph display */}
-
         <GraphDisplay />
-        <p>
-          Select from stored graphs: &nbsp; &nbsp;
-          <select id="select">
-            <option value="cohortdata">Cohort Data</option>
-            <option value="cereal">Cereal or milk first</option>
-          </select>
-        </p>
       </div>
     )
   }
--- client/components/GraphDisplay.jsx
+++ client/components/GraphDisplay.jsx
@@ -5,18 +5,19 @@ import ChartistGraph from 'react-chartist';
 
 class GraphDisplay extends React.Component {
   render() {
-
     const data = {
       labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
       series: [
-        [10, 14, 14, 12, 11, 12, 16, 12, 16, 12, 17, 23, 25]
+        [10, 14, 14, 12, 11, 12, 16, 12, 16, 12, 17, 23, 25],
+        [6, 8, 9, null, 20, null, null, 17, 18, 20, 22, 27, 29]
       ]
     };
 
     const options = {
-      high: 30,
-      low: 0,
+      // high: 30,
+      // low: 0,
       divisor: 4,
+      showArea: false,
       axisX: {
       }
     };
@@ -25,8 +26,28 @@ class GraphDisplay extends React.Component {
 
     return (
       <div>
-        <p id="graph">Graph</p>
+        <div id="graph">Graph
+        <br /> <br />
+        </div>
+
+        {/* BELOW IS STRETCH FEATURE TO SELECT STORED GRAPHS 
+        Might need to transform this into a for loop to push 
+        an option with a value attribute to 
+        store/load all saved graphs
+        */}
+        
+        <p>
+          <span id="select">
+            Select from stored graphs: &nbsp;</span>
+            <select id="select">
+                <option value="cohortdata">Cohort Data</option>
+                <option value="cereal">Cereal or milk first</option>
+            </select>
+        </p>
+
         <ChartistGraph data={data} options={options} type={type} />
+        <button id="saveBtn">Save Graph</button>
+
       </div>
     )
   }
